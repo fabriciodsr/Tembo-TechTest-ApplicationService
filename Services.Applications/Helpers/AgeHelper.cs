ebd7e9cff4bf7b8b8e1e92f0b17b426b6a624bb9
Fabricio Rodrigues <fabricio-dsr@hotmail.com>
2024-07-03 01:33:45 -0300

    AgeHelper for Applications

---
+++ Services.Applications/Helpers/AgeHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Applications.Helpers
+{
+    public class AgeHelper
+    {
+        public static int CalculateAge(DateOnly dateOfBirth)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+            int age = today.Year - dateOfBirth.Year;
+            return age;
+        }
+    }
+}
