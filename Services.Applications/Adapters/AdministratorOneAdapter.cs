58950b9915d7c8d5fcc3893bb109dfd07510ef05
Fabricio Rodrigues <fabricio-dsr@hotmail.com>
2024-07-03 01:35:33 -0300

    Adapters for the Administrator projects

---
+++ Services.Applications/Adapters/AdministratorOneAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.AdministratorOne.Abstractions;
+using Services.AdministratorOne.Abstractions.Model;
+using Services.Common.Abstractions.Abstractions;
+using Services.Common.Abstractions.Model;
+
+namespace Services.Applications
+{
+    public class AdministratorOneAdapter : IAdministrationService
+    {
+        public AdministratorOneAdapter()
+        {
+        }
+
+        public CreateInvestorResponse CreateInvestor(CreateInvestorRequest request)
+        {
+            // TODO: Handle the implementation for a third party service or something related
+
+            return new CreateInvestorResponse
+            {
+                Reference = request.Reference,
+                AccountId = Guid.NewGuid().ToString(),
+                InvestorId = Guid.NewGuid().ToString(),
+                PaymentId = Guid.NewGuid().ToString()
+            };
+        }
+    }
+}
---
+++ Services.Applications/Adapters/AdministratorTwoAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.AdministratorTwo.Abstractions;
+using Services.Common.Abstractions.Abstractions;
+using Services.Common.Abstractions.Model;
+
+namespace Services.Applications
+{
+    public class AdministratorTwoAdapter : IAdministrationService
+    {
+        public AdministratorTwoAdapter()
+        {
+        }
+
+        public Task<Result<Guid>> CreateInvestorAsync(User user)
+        {
+            // TODO: Handle the implementation for a third party service or something related
+            return Task.FromResult(Result.Success(Guid.NewGuid()));
+        }
+
+        public Task<Result<Guid>> CreateAccountAsync(Guid investorId, ProductCode productCode)
+        {
+            // TODO: Handle the implementation for a third party service or something related
+            return Task.FromResult(Result.Success(Guid.NewGuid()));
+        }
+
+        public Task<Result<Guid>> ProcessPaymentAsync(Guid accountId, Payment payment)
+        {
+            // TODO: Handle the implementation for a third party service or something related
+            return Task.FromResult(Result.Success(Guid.NewGuid()));
+        }
+    }
+}
