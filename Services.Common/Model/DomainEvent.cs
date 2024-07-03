4255ef74d308fa96b273f8cea3ed97e3ba97fb25
Fabricio Rodrigues <fabricio-dsr@hotmail.com>
2024-07-03 01:33:16 -0300

    Common additions

--- Services.Common/Model/DomainEvent.cs
+++ Services.Common/Model/DomainEvent.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Services.Common.Abstractions.Model;
 
 public abstract record DomainEvent;
@@ -9,3 +11,15 @@ public record AccountCreated(string InvestorId, ProductCode Product, string Acco
 public record KycFailed(Guid UserId, Guid ReportId) : DomainEvent;
 
 public record ApplicationCompleted(Guid ApplicationId) : DomainEvent;
+
+public record KycCompleted(Guid UserId, Guid ReportId, bool IsVerified) : DomainEvent;
+
+public record EligibilityCheckCompleted(Guid ApplicationId, ProductCode Product, bool IsEligible) : DomainEvent;
+
+public record EligibilityCheckFailed(Guid ApplicationId, ProductCode Product, string Reason) : DomainEvent;
+
+public record InvestorCreationFailed(Guid ApplicationId, ProductCode Product, string Reason) : DomainEvent;
+
+public record AccountCreationFailed(Guid ApplicationId, string InvestorId, ProductCode Product, string Reason) : DomainEvent;
+
+public record ApplicationFailed(Guid ApplicationId, ProductCode Product, string Reason) : DomainEvent;
