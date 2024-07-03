c1a5b71683ff8f62b1c41f4638ce9947c848a301
Fabricio Rodrigues <fabricio-dsr@hotmail.com>
2024-07-03 01:32:26 -0300

    Interfaces for Factory, Handler and Validator

---
+++ Services.Applications/Abstractions/IAdministrationServiceHandlerFactory.cs
@@ -0,0 +1,15 @@
+﻿using Services.Applications.Abstractions;
+using Services.Common.Abstractions.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Applications.Abstractions
+{
+    public interface IAdministrationServiceHandlerFactory
+    {
+        IAdministrationServiceHandler GetHandler(ProductCode productCode);
+    }
+}
---
+++ Services.Applications/Abstractions/IApplicationValidator.cs
@@ -0,0 +1,14 @@
+﻿using Services.Common.Abstractions.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Applications.Abstractions
+{
+    public interface IApplicationValidator
+    {
+        bool ValidateApplication(Application application, out string failureReason);
+    }
+}
