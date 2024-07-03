0c4b04302827b143f99f012c2f4d4dea5001d504
Fabricio Rodrigues <fabricio-dsr@hotmail.com>
2024-07-03 01:34:38 -0300

    Administrator handlers

---
+++ Services.Applications/Handlers/AdministratorOneHandler.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Services.AdministratorOne.Abstractions;
+using Services.AdministratorOne.Abstractions.Model;
+using Services.Applications.Abstractions;
+using Services.Common.Abstractions.Abstractions;
+using Services.Common.Abstractions.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Applications.Handlers
+{
+    public class AdministratorOneHandler : IAdministrationServiceHandler
+    {
+        private readonly IAdministrationService _service;
+        private readonly IBus _bus;
+        private readonly IMapper _mapper;
+
+        public AdministratorOneHandler(IAdministrationService service, IBus bus, IMapper mapper)
+        {
+            _service = service;
+            _bus = bus;
+            _mapper = mapper;
+        }
+
+        public async Task HandleApplicationAsync(Application application)
+        {
+            try
+            {
+                var request = _mapper.Map<CreateInvestorRequest>(application);
+                var response = _service.CreateInvestor(request);
+
+                await _bus.PublishAsync(new InvestorCreated(application.Applicant.Id, response.InvestorId));
+                await _bus.PublishAsync(new AccountCreated(response.InvestorId, application.ProductCode, response.AccountId));
+                await _bus.PublishAsync(new ApplicationCompleted(application.Id));
+            }
+            catch (AdministratorException ex)
+            {
+                switch (ex.Code)
+                {
+                    case ErrorCodes.InvestorError:
+                        await _bus.PublishAsync(new InvestorCreationFailed(application.Id, application.ProductCode, "Failed to create investor"));
+                        break;
+                    case ErrorCodes.AccountError:
+                        await _bus.PublishAsync(new AccountCreationFailed(application.Id, string.Empty, application.ProductCode, "Failed to create account"));
+                        break;
+                    case ErrorCodes.PaymentError:
+                        await _bus.PublishAsync(new ApplicationFailed(application.Id, application.ProductCode, "Failed to process payment"));
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Services.Applications/Handlers/AdministratorTwoHandler.cs
@@ -0,0 +1,51 @@
+﻿using Services.AdministratorTwo.Abstractions;
+using Services.Applications.Abstractions;
+using Services.Common.Abstractions.Abstractions;
+using Services.Common.Abstractions.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Applications.Handlers
+{
+    public class AdministratorTwoHandler : IAdministrationServiceHandler
+    {
+        private readonly IAdministrationService _service;
+        private readonly IBus _bus;
+
+        public AdministratorTwoHandler(IAdministrationService service, IBus bus)
+        {
+            _service = service;
+            _bus = bus;
+        }
+
+        public async Task HandleApplicationAsync(Application application)
+        {
+            var investorId = await _service.CreateInvestorAsync(application.Applicant);
+            if (!investorId.IsSuccess)
+            {
+                await _bus.PublishAsync(new InvestorCreationFailed(application.Id, application.ProductCode, "Failed to create investor"));
+                return;
+            }
+            await _bus.PublishAsync(new InvestorCreated(application.Applicant.Id, investorId.Value.ToString()));
+
+            var accountId = await _service.CreateAccountAsync(investorId.Value, application.ProductCode);
+            if (!accountId.IsSuccess)
+            {
+                await _bus.PublishAsync(new AccountCreationFailed(application.Id, investorId.Value.ToString(), application.ProductCode, "Failed to create account"));
+                return;
+            }
+            await _bus.PublishAsync(new AccountCreated(investorId.Value.ToString(), application.ProductCode, accountId.Value.ToString()));
+
+            var paymentResult = await _service.ProcessPaymentAsync(accountId.Value, application.Payment);
+            if (!paymentResult.IsSuccess)
+            {
+                await _bus.PublishAsync(new ApplicationFailed(application.Id, application.ProductCode, "Failed to process payment"));
+                return;
+            }
+            await _bus.PublishAsync(new ApplicationCompleted(application.Id));
+        }
+    }
+}
